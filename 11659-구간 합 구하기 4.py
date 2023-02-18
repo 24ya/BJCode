8d254aa3e67ed5478c9ed400ee7105f2532f6397
24ya <bsjp2012@hufs.ac.kr>
2023-02-19 01:03:51 +0900

    11659 구간 합 구하기 4
    [11659-구간 합 구하기 4] 완료

--- 11659-구간 합 구하기 4.py
+++ 11659-구간 합 구하기 4.py
@@ -1,10 +1,17 @@
 #11659 - 구간합 구하기 4
+import sys
+input=sys.stdin.readline
 N,M=map(int,input().split())
-A=tuple(map(int,input().split()))
+A=list(map(int,input().split()))
+garage=[0]*N
+for i in range(N):
+    garage[i]=garage[i-1]+A[i]
+
 for i in range(M):
-    s,f=map(int,input().split())
-    s-=1
-    total=0
-    for j in range(s,f):
-        total+=A[j]
-    print(total)
+    S, F = map(int, input().split())
+    S-=2
+    F-=1
+    if S<0:
+        print(garage[F])
+    else:
+        print(garage[F]-garage[S])
